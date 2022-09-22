302c545b2e71faf87ddb957b72c6f3f9240a1a0b
Igor Marchi <igoraugustomarchi@gmail.com>
2022-09-22 01:46:54 -0300

    create TransactionsTable

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,50</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td>Sushi</td>
+              <td>
+                <PriceHighlight variant="outcome">- 12.000,50</PriceHighlight>
+              </td>
+              <td>Alimento</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
